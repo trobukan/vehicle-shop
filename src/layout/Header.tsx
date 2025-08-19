bccd0ce1ebc01b7d6b5e1d20613b0a3d6206f8a1
trobukan <trobukan@gmail.com>
2025-08-19 17:02:37 -0300

    feat: Add Header layout

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,9 +1,15 @@
+import type { ReactNode } from "react";
+import headerBackground from "../assets/header__background.png"
+
 interface Props {
   className?: string;
+  children?: ReactNode;
 }
 
 const Header = ({ className }: Props) => {
-  return <header className={className}>Header</header>;
+  return <header className={className}>
+    <img className="header__image" src={headerBackground} alt="" />
+  </header>;
 };
 
 export default Header;
