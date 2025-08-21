649d2c7aa256881c6fb3388c12c0c0cb35f1922c
trobukan <trobukan@gmail.com>
2025-08-21 16:14:27 -0300

    fix: Replaced to HashRouter, more appropriate for static pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,14 @@ import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
+import { CarProvider } from "./contexts/CarContext.tsx";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
+      <CarProvider>
       <App />
+      </CarProvider>
     </HashRouter>
   </StrictMode>
 );
