a89a6bcb8ef052ba8592a9f2e5c9e5d07a981302
trobukan <trobukan@gmail.com>
2025-08-21 16:56:26 -0300

    feat: Scroll until ID element

--- src/layouts/Nav.tsx
+++ src/layouts/Nav.tsx
@@ -2,6 +2,12 @@ import LogoSVG from "../components/LogoSVG";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    const mainRoot = document.querySelector("#models");
+    mainRoot?.scrollIntoView({ behavior: "smooth", block: "center" });
+  };
+
   return (
     <nav>
       <div className="nav-container">
@@ -10,11 +16,14 @@ const Nav = () => {
         </Link>
 
         <div className="nav__links">
-          <Link to={"/"} className="nav__link-item">Home</Link>
-          <a href="#models" className="nav__link-item">Models</a>
+          <Link to={"/"} className="nav__link-item">
+            Home
+          </Link>
+          <a onClick={handleClick} href="#" className="nav__link-item">
+            Models
+          </a>
         </div>
       </div>
-      
     </nav>
   );
 };
